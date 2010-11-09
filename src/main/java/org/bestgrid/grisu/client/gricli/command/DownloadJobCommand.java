bc2ca5c62035b80d32eee33f5a9eeefc78402008
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2010-11-09 18:04:57 +1300

    Refactoring: deleted DtoFolder and DtoFile and replaced with DtoFileObject.

--- src/main/java/org/bestgrid/grisu/client/gricli/command/DownloadJobCommand.java
+++ src/main/java/org/bestgrid/grisu/client/gricli/command/DownloadJobCommand.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
+import java.util.Set;
+
 import javax.activation.DataHandler;
+
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
 import org.bestgrid.grisu.client.gricli.GricliEnvironment;
@@ -14,75 +16,84 @@ import org.bestgrid.grisu.client.gricli.util.ServiceInterfaceUtils;
 import org.vpac.grisu.control.ServiceInterface;
 import org.vpac.grisu.control.exceptions.NoSuchJobException;
 import org.vpac.grisu.control.exceptions.RemoteFileSystemException;
-import org.vpac.grisu.model.dto.DtoFile;
-import org.vpac.grisu.model.dto.DtoFolder;
+import org.vpac.grisu.model.dto.DtoFileObject;
 import org.vpac.grisu.model.dto.DtoJob;
 
+public class DownloadJobCommand implements GricliCommand {
+	private final String jobFilter;
 
-public class DownloadJobCommand implements GricliCommand{
-    private final String jobFilter;
+	public DownloadJobCommand(String jobFilter) {
+		this.jobFilter = jobFilter;
+	}
 
-    public DownloadJobCommand(String jobFilter){
-        this.jobFilter = jobFilter;
-    }
+	private void download(ServiceInterface si, DtoFileObject df, File dst)
+			throws RemoteFileSystemException, IOException {
+		Set<DtoFileObject> files = df.getChildren();
+		for (DtoFileObject file : files) {
+			DataHandler dh = si.download(file.getMainUrl());
+			InputStream in = dh.getInputStream();
+			FileOutputStream fout = new FileOutputStream(FilenameUtils.concat(
+					dst.getCanonicalPath(), file.getName()));
+			byte buf[] = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				fout.write(buf, 0, len);
+			}
+			fout.close();
+			in.close();
+		}
 
-    public GricliEnvironment execute(GricliEnvironment env) throws GricliRuntimeException {
+		Set<DtoFileObject> folders = df.getChildren();
+		for (DtoFileObject folder : folders) {
+			File dst2 = new File(FilenameUtils.concat(dst.getCanonicalPath(),
+					folder.getName()));
+			dst2.mkdir();
+			download(si, folder, dst2);
+		}
+	}
 
-            ServiceInterface si = env.getServiceInterface();
-            String normalDirName = StringUtils.replace(env.get("dir"), "~", System.getProperty("user.home"));
-            for (String jobname: ServiceInterfaceUtils.filterJobNames(si, jobFilter)){
-                downloadJob(si,jobname,FilenameUtils.concat(normalDirName, jobname));
-            }
-            return env;
-      
-    }
+	private void downloadDir(String src, String dst, ServiceInterface si)
+			throws GricliRuntimeException {
+		try {
+			File dir = new File(dst);
+			dir.mkdir();
 
-    private void downloadJob(ServiceInterface si, String jobname, String dst) throws GricliRuntimeException {
-        try {
-            System.out.println("downloading job " + jobname);
-            DtoJob job = si.getJob(jobname);
-            downloadDir(job.jobProperty("jobDirectory"),dst, si);
-        } catch (NoSuchJobException ex) {
-            throw new GricliRuntimeException("job " + jobname + " does not exist");
-        }
-    }
+			download(si, si.ls(src, 1), dir);
+		} catch (RemoteFileSystemException ex) {
+			throw new GricliRuntimeException(
+					" cannot access remote file system: " + ex.getMessage());
+		} catch (IOException ex) {
+			throw new GricliRuntimeException(
+					"cannot access local file system: " + ex.getMessage());
+		}
 
+	}
 
-    private void downloadDir(String src, String dst, ServiceInterface si) throws GricliRuntimeException{
-        try {
-            File dir = new File(dst);
-            dir.mkdir();
+	private void downloadJob(ServiceInterface si, String jobname, String dst)
+			throws GricliRuntimeException {
+		try {
+			System.out.println("downloading job " + jobname);
+			DtoJob job = si.getJob(jobname);
+			downloadDir(job.jobProperty("jobDirectory"), dst, si);
+		} catch (NoSuchJobException ex) {
+			throw new GricliRuntimeException("job " + jobname
+					+ " does not exist");
+		}
+	}
 
-            download(si,si.ls(src, 1),dir);
-        } catch (RemoteFileSystemException ex) {
-           throw new GricliRuntimeException(" cannot access remote file system: " + ex.getMessage());
-        } catch (IOException ex) {
-            throw new GricliRuntimeException("cannot access local file system: " + ex.getMessage());
-        }
+	public GricliEnvironment execute(GricliEnvironment env)
+			throws GricliRuntimeException {
 
-    }
+		ServiceInterface si = env.getServiceInterface();
+		String normalDirName = StringUtils.replace(env.get("dir"), "~",
+				System.getProperty("user.home"));
+		for (String jobname : ServiceInterfaceUtils.filterJobNames(si,
+				jobFilter)) {
+			downloadJob(si, jobname,
+					FilenameUtils.concat(normalDirName, jobname));
+		}
+		return env;
 
-    private void download(ServiceInterface si, DtoFolder df, File dst) throws RemoteFileSystemException, IOException{
-      List<DtoFile> files = df.getChildrenFiles();
-      for (DtoFile file: files){
-          DataHandler dh = si.download(file.getRootUrl());
-          InputStream in = dh.getInputStream();
-          FileOutputStream fout = new FileOutputStream(FilenameUtils.concat(dst.getCanonicalPath(),file.getName()));
-          byte buf[] = new byte[1024];
-          int len;
-          while ((len = in.read(buf))>0){
-              fout.write(buf,0,len);
-          }
-          fout.close();
-          in.close();
-      }
-
-      List<DtoFolder> folders = df.getChildrenFolders();
-      for (DtoFolder folder: folders){
-          File dst2 = new File(FilenameUtils.concat(dst.getCanonicalPath(),folder.getName()));
-          dst2.mkdir();
-          download(si,folder,dst2);
-      }
-    }
+	}
 
 }
--- src/main/java/org/bestgrid/grisu/client/gricli/command/GridLsCommand.java
+++ src/main/java/org/bestgrid/grisu/client/gricli/command/GridLsCommand.java
@@ -4,28 +4,28 @@ import org.bestgrid.grisu.client.gricli.GricliEnvironment;
 import org.bestgrid.grisu.client.gricli.GricliRuntimeException;
 import org.vpac.grisu.control.ServiceInterface;
 import org.vpac.grisu.control.exceptions.RemoteFileSystemException;
-import org.vpac.grisu.model.dto.DtoFile;
-import org.vpac.grisu.model.dto.DtoFolder;
+import org.vpac.grisu.model.dto.DtoFileObject;
 
-public class GridLsCommand implements GricliCommand{
+public class GridLsCommand implements GricliCommand {
 
-    public GricliEnvironment execute(GricliEnvironment env) throws GricliRuntimeException {
-        ServiceInterface si = env.getServiceInterface();
-        try {
-            String url = "gsiftp://" + env.get("host") + env.get("gdir");
-            DtoFolder folder = si.ls(url, 1);
-            for (DtoFile file: folder.getChildrenFiles()){
-                System.out.println(file.getName());
-            }
+	public GricliEnvironment execute(GricliEnvironment env)
+			throws GricliRuntimeException {
+		ServiceInterface si = env.getServiceInterface();
+		try {
+			String url = "gsiftp://" + env.get("host") + env.get("gdir");
+			DtoFileObject folder = si.ls(url, 1);
+			for (DtoFileObject file : folder.getChildren()) {
+				if (file.isFolder()) {
+					System.out.println(file.getName());
+				} else {
+					System.out.println(file.getName() + "/");
+				}
+			}
 
-            for (DtoFolder file: folder.getChildrenFolders()){
-                System.out.println(file.getName() + "/");
-            }
-
-        } catch (RemoteFileSystemException ex) {
-            throw new GricliRuntimeException(ex);
-        }
-        return env;
-    }
+		} catch (RemoteFileSystemException ex) {
+			throw new GricliRuntimeException(ex);
+		}
+		return env;
+	}
 
 }
