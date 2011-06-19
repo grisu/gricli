9cdb7d6a368b685b8606f4d88916f48a154274a1
Yuriy Halytskyy <yuriy.halytskyy@gmail.com>
2011-06-20 10:42:25 +1200

    destroy proxy command does not require login session

--- src/main/java/grisu/gricli/command/LogoutCommand.java
+++ src/main/java/grisu/gricli/command/LogoutCommand.java
@@ -2,6 +2,7 @@ package grisu.gricli.command;
 
 import grisu.gricli.GricliEnvironment;
 import grisu.gricli.GricliRuntimeException;
+import grisu.gricli.LoginRequiredException;
 
 import java.io.File;
 
@@ -16,7 +17,11 @@ GricliCommand {
 
 	public GricliEnvironment execute(GricliEnvironment env)
 	throws GricliRuntimeException {
-		env.getServiceInterface().logout();
+		try {
+			env.getServiceInterface().logout();
+		} catch (LoginRequiredException l){
+			// do nothing as login session does not exist.
+		}
 		String proxy = org.globus.common.CoGProperties.getDefault().getProxyFile();
 		new File(proxy).delete();
 		return env;
