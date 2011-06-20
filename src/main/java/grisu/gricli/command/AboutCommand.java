261d5fcd31ba6af7f9dd1c63380a78fb1717563c
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-06-20 09:59:40 +0200

    More info for about command.

--- src/main/java/grisu/gricli/command/AboutCommand.java
+++ src/main/java/grisu/gricli/command/AboutCommand.java
@@ -4,13 +4,21 @@ import grisu.gricli.GricliEnvironment;
 import grisu.gricli.GricliRuntimeException;
 
 public class AboutCommand implements GricliCommand {
-	
+
 	@SyntaxDescription(command={"about"})
 	public AboutCommand(){}
 
 	public GricliEnvironment execute(GricliEnvironment env)
-			throws GricliRuntimeException {
+	throws GricliRuntimeException {
 		env.printMessage("version: " + grisu.jcommons.utils.Version.get("gricli"));
+		env.printMessage("grisu frontend version: "
+				+ grisu.jcommons.utils.Version.get("grisu-client"));
+		env.printMessage("grisu backend: "
+				+ env.getServiceInterface().getInterfaceInfo("NAME"));
+		env.printMessage("grisu backend host: "
+				+ env.getServiceInterface().getInterfaceInfo("HOSTNAME"));
+		env.printMessage("grisu backend version: "
+				+ env.getServiceInterface().getInterfaceInfo("VERSION"));
 		env.printMessage("documentation: https://github.com/grisu/gricli");
 		env.printMessage("contact: eresearch-admin@list.auckland.ac.nz");
 		return env;
