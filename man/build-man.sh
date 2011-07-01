3f72661494156628c9d7fd3050547f03b847f18e
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-07-01 18:45:19 +0200

    Added manpage template and manpage generator script. Needs bit more work, but only documentation.

---
+++ man/build-man.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# builds the gricli manpage, uses the help.properties 
+# file to get descriptions for all commands
+# needs pandoc installed (http://johnmacfarlane.net/pandoc/index.html)
+# also sed, awk
+
+GRICLI_DIR=$1
+HELPFILE=$GRICLI_DIR/src/main/resources/help.properties
+
+TEMPFILE=/tmp/gricli.md
+
+cat $GRICLI_DIR/man/manpage.md > $TEMPFILE
+
+while read line
+do 
+    COMMAND=${line%=*}
+    DESC=${line#*=}
+    CLEAN=`echo $COMMAND|sed 's/\./ /g'`
+    STRIPPED=`echo $CLEAN| awk '{gsub(/^ +| +$/,"")}1'`
+    if [  -n "$STRIPPED" ]
+    then
+	echo "\`$STRIPPED\`" >> $TEMPFILE
+	echo ":    $DESC" >> $TEMPFILE
+	echo >> $TEMPFILE
+    fi
+
+done < $HELPFILE
+
+echo 'The Gricli source code and all documentation may be downloaded from
+<http://github.com/grisu/gricli>.' >> $TEMPFILE
+
+
+pandoc -s -w man $TEMPFILE -o $GRICLI_DIR/man/gricli.1
+
