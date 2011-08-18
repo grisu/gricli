46f3f9075c5aa37ab791b255cafa8e4df8b5aedd
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-08-18 12:34:40 +0200

    Commented out body of GetCommand, breaks build. No idea what that is supposed to do, actually.

--- src/main/java/grisu/gricli/command/GetCommand.java
+++ src/main/java/grisu/gricli/command/GetCommand.java
@@ -1,11 +1,7 @@
 package grisu.gricli.command;
 
-import grisu.control.ServiceInterface;
-import grisu.control.exceptions.RemoteFileSystemException;
 import grisu.gricli.GricliRuntimeException;
 import grisu.gricli.environment.GricliEnvironment;
-
-import javax.activation.DataHandler;
 
 @SuppressWarnings("restriction")
 public class GetCommand implements GricliCommand {
@@ -17,16 +13,16 @@ public class GetCommand implements GricliCommand {
 
 	@SyntaxDescription(command={"get"})
 	public GricliEnvironment execute(GricliEnvironment env)
-	throws GricliRuntimeException {
-		String url = "gsiftp://" + env.host.get() + env.gdir.get() + "/"
-		+ file;
-		ServiceInterface si = env.getServiceInterface();
-		try {
-			DataHandler result = si.download(url);
-
-		} catch (RemoteFileSystemException ex) {
-			throw new GricliRuntimeException(ex);
-		}
+			throws GricliRuntimeException {
+		// String url = "gsiftp://" + env.host.get() + env.gdir.get() + "/"
+		// + file;
+		// ServiceInterface si = env.getServiceInterface();
+		// try {
+		// DataHandler result = si.download(url);
+		//
+		// } catch (RemoteFileSystemException ex) {
+		// throw new GricliRuntimeException(ex);
+		// }
 		return env;
 	}
 
