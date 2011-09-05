df9b0461c5d31b30235c9a5e47c1e661463767ed
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-09-05 08:52:36 +0200

    Added Files topic to list of topics

--- src/main/java/grisu/gricli/command/help/HelpManager.java
+++ src/main/java/grisu/gricli/command/help/HelpManager.java
@@ -45,7 +45,7 @@ public class HelpManager {
 	private static Logger myLogger = Logger.getLogger(HelpManager.class
 			.getName());
 
-	public static String[] TOPICS = new String[] { "Globals", "Jobs"};
+	public static String[] TOPICS = new String[] { "Globals", "Jobs", "Files" };
 
 	public static String getHelpText(Type type, String s) throws Exception {
 		InputStream is = null;
@@ -163,7 +163,7 @@ public class HelpManager {
 				missingCommands.add(cmd);
 			}
 		}
-				
+
 		for (String global : GricliEnvironment.getVariableNames()) {
 			String temp;
 			try {
