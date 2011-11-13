01757f8477c39897fd0fbc039f48d7aa0657a054
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-11-14 09:35:22 +1300

    Renamed destroy proxy command to close session

--- src/main/java/grisu/gricli/command/LogoutCommand.java
+++ src/main/java/grisu/gricli/command/LogoutCommand.java
@@ -8,7 +8,7 @@ import java.io.File;
 
 public class LogoutCommand implements GricliCommand {
 
-	@SyntaxDescription(command = { "destroy", "proxy" })
+	@SyntaxDescription(command = { "close", "session" })
 	public LogoutCommand() {
 		super();
 	}
