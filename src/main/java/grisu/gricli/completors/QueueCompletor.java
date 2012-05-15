f68f8752d25aee0c8115fa9e5e32a9a2a144fc4d
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-05-15 14:39:36 +1200

    Minor.

--- src/main/java/grisu/gricli/completors/QueueCompletor.java
+++ src/main/java/grisu/gricli/completors/QueueCompletor.java
@@ -5,7 +5,7 @@ import grisu.gricli.GricliRuntimeException;
 import grisu.gricli.command.PrintQueuesCommand;
 import grisu.gricli.environment.GricliEnvironment;
 import grisu.jcommons.constants.Constants;
-import grisu.model.info.dto.Queue;
+import grisu.model.info.dto.JobQueueMatch;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -30,7 +30,7 @@ public class QueueCompletor implements Completor {
 
 		final GricliEnvironment env = Gricli.completionCache.getEnvironment();
 
-		List<Queue> queues;
+		List<JobQueueMatch> queues;
 		try {
 			queues = PrintQueuesCommand.calculateAllAvailableQueues(env);
 		} catch (GricliRuntimeException e) {
